f4e2fa16f90d525a8f294f98ebc9150393f0492b
Gajender <Iamsanju0707@gmail.com>
2024-09-10 15:41:21 +0530

    adding the files

--- restaurant/order_food/urls.py
+++ restaurant/order_food/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    path("",views.home,name="home")
+    path("",views.home,name="home"),
+    path("login/",views.login,name="login"),
+    path("register/",views.register,name="register"),
+    path("contact_us/",views.contact_us,name="contact_us"),
+    path("rough/",views.rough,name="rough"),
+    path("pizza/",views.pizza,name="pizza"),
+    path("drink/",views.drink,name="drink"),
+    path("dessert/",views.dessert,name="dessert"),
 ]
--- restaurant/order_food/views.py
+++ restaurant/order_food/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,"index.html")
+
+
+def login(request):
+    return render(request,"login.html")
+
+
+def register(request):
+    return render(request,"register.html")
+
+
+def contact_us(request):
+    return render(request,"contact_us.html")
+
+
+def rough(request):
+    return render(request,"rough.html")
+
+
+def pizza(request):
+    return render(request,"pizza.html")
+
+
+def drink(request):
+    return render(request,"drink.html")
+
+
+def dessert(request):
+    return render(request,"dessert.html")
