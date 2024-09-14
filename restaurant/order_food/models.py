8c3d5fd726cb069c81c52d653f34912c35181c55
Gajender <Iamsanju0707@gmail.com>
2024-09-14 12:02:04 +0530

    adding the files

--- restaurant/order_food/models.py
+++ restaurant/order_food/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class customer_details(models.Model):
-    name=models.CharField(max_length=100,null=True)
-    email=models.EmailField(null=True)
-
 
 class order_details(models.Model):
+
+    #creating the user detaisl for the login and resgitering the details...
+    user=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    
+    #creating the feilds for the adding the details of the Order....
     order_id=models.IntegerField(null=False)
     item_name=models.CharField(max_length=100,null=True)
     item_quantity=models.IntegerField(null=True)
